709bbdc18e67a24e1ac0ceb36cde23c2c468b641
diegoxvega <diegoxvega@gmail.com>
2026-06-25 16:10:33 -0400

    Update apps.py

--- ordenes_trabajo/apps.py
+++ ordenes_trabajo/apps.py
@@ -4,3 +4,18 @@ from django.apps import AppConfig
 class OrdenesTrabajoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ordenes_trabajo'
+
+    def ready(self):
+        from .models import OrdenTrabajo
+
+        if hasattr(OrdenTrabajo, 'puede_editar_observaciones_tecnicas'):
+            return
+
+        def puede_editar_observaciones_tecnicas(self, usuario):
+            if usuario.rol in ['ADMIN', 'ADMINISTRATIVO']:
+                return True
+            if usuario.rol == 'TECNICO' and usuario == self.tecnico_responsable:
+                return True
+            return False
+
+        OrdenTrabajo.puede_editar_observaciones_tecnicas = puede_editar_observaciones_tecnicas
